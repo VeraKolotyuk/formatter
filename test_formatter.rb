9d21d13f398a2aaa53ad17c8b71f6dbbbb03ba08
Vera <vera@vera-K50IN.(none)>
2013-09-23 21:22:00 +0700

    test method refactoring

--- test_formatter.rb
+++ test_formatter.rb
@@ -13,15 +13,21 @@ class TestFormatter < Test::Unit::TestCase
   end
 
   def test_correct
-    File.open('test-data/source-correct.txt','r') do |input|
-        File.open('test-data/target-test-correct.txt', 'w') do |output|
+    format('test-data/source-correct.txt', 'test-data/target-test-correct.txt')
+    assert_equal(FileUtils.compare_file('test-data/target-test-correct.txt', 'test-data/target-correct.txt'), true)
+  end
+
+  private
+
+  def format(source_path, target_path)
+    File.open(source_path,'r') do |input|
+        File.open(target_path, 'w') do |output|
             reader = InputStreamReader.new(input)
             writer = OutputStreamWriter.new(output)
             formatter = Formatter.new
             formatter.format(reader, writer)
         end
     end
-    assert_equal(FileUtils.compare_file('test-data/target-test-correct.txt', 'test-data/target-correct.txt'), true)
   end
 
 end
